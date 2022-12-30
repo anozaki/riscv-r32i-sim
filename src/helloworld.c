29e640a3cc1181a7fcdad1764bb22fd8f7d26906
Akito Nozaki <akito.nozaki@kibsi.com>
2022-12-29 21:31:03 -0800

    console output

--- src/helloworld.c
+++ src/helloworld.c
@@ -1,8 +1,19 @@
+#include <stdint.h>
+#include <string.h>
 
-extern void memoryMapGPIOTest();
+#define CONSOLE (void *)0x03000000
+
 int main(void)
 {
-    memoryMapGPIOTest();
+    // 80x24 console
+    uint8_t *console = CONSOLE;
+    const char *message = "Hello World! compiled application!!";
+    uint8_t size = strlen(message);
+    for (int j = 0; j < 24; j++) {
+        for (int i = 0; i < size; i++) {
+            console[j * 80 + i] = message[i];
+        }
+    }
     return 0;
 }
 
